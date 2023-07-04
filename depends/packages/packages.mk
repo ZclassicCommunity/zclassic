6ad040e67c7e0bbde5a188fc9d2a211b89c0657a
Rhett Creighton <>
2023-07-04 02:52:10 -0400

    adding packages

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -34,8 +34,23 @@ rust_crates := \
   crate_winapi_x86_64_pc_windows_gnu
 rust_packages := rust $(rust_crates) librustzcash
 proton_packages := proton
-zcash_packages := libgmp libsodium
+zcash_packages := libsodium
 packages := boost openssl libevent zeromq $(zcash_packages) googletest libcurl
-native_packages := native_clang native_ccache native_cmake native_cxxbridge native_zstd
+native_packages := native_clang native_ccache native_cmake native_zstd
+
+ifneq (,$(wildcard /etc/arch-release))
+native_packages += native_libtinfo
+endif
 
 wallet_packages=bdb
+
+$(host_arch)_$(host_os)_native_packages += native_b2
+
+ifneq ($(build_os),darwin)
+darwin_native_packages=native_cctools
+endif
+
+# We use a complete SDK for Darwin, which includes libc++.
+ifneq ($(host_os),darwin)
+packages += libcxx
+endif
