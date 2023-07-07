a1f81d0b45811bdaecc9820500a6a3d911e641ca
Rhett Creighton <>
2023-07-07 01:49:13 -0400

    boost update

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -5,23 +5,23 @@ $(package)_file_name=$(package)_$($(package)_version).tar.bz2
 $(package)_sha256_hash=1e19565d82e43bc59209a168f5ac899d3ba471d55c7610c677d4ccf2c9c500c0
 
 define $(package)_set_vars
-  $(package)_config_opts_release=variant=release
-  $(package)_config_opts_debug=variant=debug
-  $(package)_config_opts=--layout=system --user-config=user-config.jam
-  $(package)_config_opts+=threading=multi link=static -sNO_BZIP2=1 -sNO_ZLIB=1
-  $(package)_config_opts_linux=threadapi=pthread runtime-link=shared
-  $(package)_config_opts_darwin=--toolset=darwin-4.2.1 runtime-link=shared
-  $(package)_config_opts_mingw32=binary-format=pe target-os=windows threadapi=win32 runtime-link=static
-  $(package)_config_opts_x86_64_mingw32=address-model=64
-  $(package)_config_opts_i686_mingw32=address-model=32
-  $(package)_config_opts_i686_linux=address-model=32 architecture=x86
-  $(package)_toolset_$(host_os)=gcc
-  $(package)_archiver_$(host_os)=$($(package)_ar)
-  $(package)_toolset_darwin=darwin
-  $(package)_archiver_darwin=$($(package)_libtool)
-  $(package)_config_libraries=chrono,filesystem,program_options,system,thread,test
-  $(package)_cxxflags=-std=c++11 -fvisibility=hidden
-  $(package)_cxxflags_linux=-fPIC
+$(package)_config_opts_release=variant=release
+$(package)_config_opts_debug=variant=debug
+$(package)_config_opts=--layout=system --user-config=user-config.jam
+$(package)_config_opts+=threading=multi link=static -sNO_BZIP2=1 -sNO_ZLIB=1
+$(package)_config_opts_linux=threadapi=pthread runtime-link=shared
+$(package)_config_opts_darwin=--toolset=darwin-4.2.1 runtime-link=shared
+$(package)_config_opts_mingw32=binary-format=pe target-os=windows threadapi=win32 runtime-link=static
+$(package)_config_opts_x86_64_mingw32=address-model=64
+$(package)_config_opts_i686_mingw32=address-model=32
+$(package)_config_opts_i686_linux=address-model=32 architecture=x86
+$(package)_toolset_$(host_os)=gcc
+$(package)_archiver_$(host_os)=$($(package)_ar)
+$(package)_toolset_darwin=darwin
+$(package)_archiver_darwin=$($(package)_libtool)
+$(package)_config_libraries=chrono,filesystem,program_options,system,thread,test
+$(package)_cxxflags=-std=c++11 -fvisibility=hidden
+$(package)_cxxflags_linux=-fPIC
 endef
 
 
