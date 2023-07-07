95c80cc273f1412393760f7a2bd13de183bb6408
Rhett Creighton <>
2023-07-07 05:04:54 -0400

    package update

--- depends/packages/libgmp.mk
+++ depends/packages/libgmp.mk
@@ -3,8 +3,13 @@ $(package)_version=6.1.1
 $(package)_download_path=https://gmplib.org/download/gmp/
 $(package)_file_name=gmp-$($(package)_version).tar.bz2
 $(package)_sha256_hash=a8109865f2893f1373b0a8ed5ff7429de8db696fc451b1036bd7bdf95bbeffd6
-$(package)_dependencies=
+$(package)_dependencies=native_cmake native_clang native_cctools
 $(package)_config_opts=--enable-cxx --disable-shared
+
+
+ifneq ($(host_os),darwin)
+$(package)_dependencies=libcxx
+endif
 
 define $(package)_config_cmds
   $($(package)_autoconf) --host=$(host) --build=$(build)
--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -36,6 +36,24 @@ rust_packages := rust $(rust_crates) librustzcash
 proton_packages := proton
 zcash_packages := libgmp libsodium
 packages := boost openssl libevent zeromq $(zcash_packages) googletest libcurl
-native_packages := native_clang native_ccache native_cmake native_cxxbridge native_zstd
+native_packages := native_clang native_ccache native_cmake native_zstd
 
 wallet_packages=bdb
+
+ifneq (,$(wildcard /etc/arch-release))
+native_packages += native_libtinfo
+endif
+
+wallet_packages=bdb
+
+$(host_arch)_$(host_os)_native_packages += native_b2
+
+ifneq ($(build_os),darwin)
+darwin_native_packages=native_cctools
+endif
+
+
+# We use a complete SDK for Darwin, which includes libc++.
+ifneq ($(host_os),darwin)
+packages += libcxx
+endif
